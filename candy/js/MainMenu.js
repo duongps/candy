4f651dc16bbd64b3c40a7a446356b90edc2ea111
duongps <duongps@viettel.com>
2017-03-14 14:36:18 +0700

    Update

--- candy/js/MainMenu.js
+++ candy/js/MainMenu.js
@@ -3,6 +3,10 @@ Candy.MainMenu = function(game){
 
 Candy.MainMenu.prototype = {
 	create: function(){
+			//add button start
+			this.btnStart = this.add.button(this.world.centerX, 400, 'btnStart', this.startGame, this, 1, 0, 2);
+			this.btnStart.anchor.set(0.5);
+			this.btnStart.inputEnabled = true;
 	},
 
 	startGame: function() {
--- candy/js/Preloader.js
+++ candy/js/Preloader.js
@@ -5,7 +5,15 @@ Candy.Preloader.prototype = {
 	preload: function(){
 		// set background color and preload image
 		this.stage.backgroundColor = '#2FC8DD';
+
+		this.load.spritesheet('btnStart','img/btnstart.png', 220, 50);
+
+		this.game.load.image('blue', 'img/gemblue.png');
+    this.game.load.image('green', 'img/gemgreen.png');
+    this.game.load.image('red', 'img/gemred.png');
+    this.game.load.image('yellow', 'img/gemyellow.png');
 	},
+
 	create: function(){
 		// start the MainMenu state
 		this.state.start('MainMenu');
